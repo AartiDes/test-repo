8c986f4a52beeb52e6a7d08c91186deacf95a919
Aarti Deshpande <arti.deshpande.ad@gmail.com>
2016-07-07 11:01:03 +0530

    Added App.java

---
+++ App.java
@@ -0,0 +1,28 @@
+import com.datametica.test;
+
+public class App{
+	public static void main(String [args]){
+		int i = 1;
+		While(i<6){
+		switch(i)
+		{
+			case 1: 
+				System.out.println("_ _ _ _*");
+				break;
+			case 2:
+				System.out.println("_ _ _* *");
+				break;
+			case 3:
+				System.out.println("_ _ * * *");
+				break;
+			case 4:
+				System.out.println("_ * * * *");
+				break;
+			case 5:
+				System.out.println("* * * * *");
+		}
+		}
+	}
+}
+
+
